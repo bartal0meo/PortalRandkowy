85841e1b66af4b8bea85b2c1a144192f1301bd23
Bartek L <bartal0meoo@gmail.com>
2020-10-28 00:16:38 +0100

    Wyświetlanie danych z bazy na stronie

--- PortalRandkowy.API/Startup.cs
+++ PortalRandkowy.API/Startup.cs
@@ -31,6 +31,7 @@ namespace PortalRandkowy.API
             //DefaultConnection pobieramy z pliku appsettings.json
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +42,9 @@ namespace PortalRandkowy.API
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             app.UseMvc();
+            
         }
     }
 }
