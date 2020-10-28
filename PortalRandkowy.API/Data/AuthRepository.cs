3e536774e30d06cff0033acfe0175b468dc49558
Bartek L <bartal0meoo@gmail.com>
2020-10-28 23:45:26 +0100

    Utworzenie metod logowania

--- PortalRandkowy.API/Data/AuthRepository.cs
+++ PortalRandkowy.API/Data/AuthRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using PortalRandkowy.API.Models;
 
 namespace PortalRandkowy.API.Data
@@ -15,10 +16,19 @@ namespace PortalRandkowy.API.Data
             _context = context;
 
         }
-        public Task<User> Login(string username, string password)
+        public async Task<User> Login(string username, string password)
         {
-            throw new System.NotImplementedException();
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == username);
+
+            if(user == null)
+                return null;
+
+            if(!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+                return null;
+
+            return user;
         }
+
 
         public async Task<User> Register(User user, string password)
         {
@@ -28,16 +38,19 @@ namespace PortalRandkowy.API.Data
 
             user.PasswordHash = passwordHash;
             user.PasswordSalt = passwordSalt;
-            
+
             await _context.Users.AddAsync(user);
             await _context.SaveChangesAsync();
 
             return user;
         }
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string username)
         {
-            throw new System.NotImplementedException();
+            if(await _context.Users.AnyAsync(x => x.UserName == username))
+                return true;
+
+            return false;
         }
         #endregion
 
@@ -48,6 +61,23 @@ namespace PortalRandkowy.API.Data
             {
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+            
+        }
+
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computtedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                for (int i = 0; i < computtedHash.Length; i++)
+                {
+                    if (computtedHash[i] != passwordHash[i])
+                        return false;
+                }
+                return true;
             }
             
         }
