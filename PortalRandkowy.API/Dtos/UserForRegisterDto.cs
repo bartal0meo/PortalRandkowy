a70a4c01dbd8e1f35db29b25018fb2c6a28cd827
Bartek L <bartal0meoo@gmail.com>
2020-10-29 22:24:50 +0100

    Walidacja logowania

--- PortalRandkowy.API/Dtos/UserForRegisterDto.cs
+++ PortalRandkowy.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PortalRandkowy.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required(ErrorMessage="Nazwa użytkownika jest wymagana")]
         public string Username { get; set; }
+        [Required(ErrorMessage="Hasło użytkownika jest wymagane")]
+        [StringLength(12,MinimumLength=6, ErrorMessage="Hasło musi się składać od 4 do 8 znaków")]
         public string Password { get; set; }
     }
 }
