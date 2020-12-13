9352070f0ed81da7837700f88f48cf8f1b058185
Bartek L <bartal0meoo@gmail.com>
2020-12-14 00:02:50 +0100

    Dodanie angular-jwt (biblioteko do tokena)

--- PortalRandkowy.SPA/src/app/_services/auth.service.ts
+++ PortalRandkowy.SPA/src/app/_services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,7 @@ import { map } from 'rxjs/operators';
 export class AuthService {
 
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
 
 constructor(private http: HttpClient) { }
 
@@ -24,6 +26,11 @@ login(model: any){
 // tslint:disable-next-line: typedef
 register(model: any) {
   return this.http.post(this.baseUrl + 'register', model);
+}
+
+loggedIn(){
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token);
 }
 
 }
